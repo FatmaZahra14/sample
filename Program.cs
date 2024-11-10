9874cc39b4e87a83100a3254a38deca9dafe98f0
FatmaZahra14 <abdultalib.fd739@s.msumain.edu.ph>
2024-11-11 01:02:24 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!, ano na");
+            Console.WriteLine("Hello, World!, ano na"); 
         }
     }
 }
